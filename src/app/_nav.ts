df9ea9eacd949437d4beb0e28262d72f3f7fb36a
Christian G <Chris@LENOVO>
2020-01-21 08:29:07 -0700

    first commit

---
+++ src/app/_nav.ts
@@ -0,0 +1,85 @@
+import { INavData } from '@coreui/angular';
+
+export const navItems: INavData[] = [
+  {
+    name: 'Dashboard',
+    url: '/dashboard',
+    icon: 'icon-speedometer',
+    badge: {
+      variant: 'info',
+      text: 'INFO'
+    }
+  },
+  {
+    title: true,
+    name: 'Administration'
+  },
+  {
+    name: 'Base',
+    url: '/base',
+    icon: 'icon-puzzle',
+    children: [
+      {
+        name: 'Cards',
+        url: '/base/cards',
+        icon: 'icon-puzzle'
+      },
+      {
+        name: 'Carousels',
+        url: '/base/carousels',
+        icon: 'icon-puzzle'
+      },
+      {
+        name: 'Collapses',
+        url: '/base/collapses',
+        icon: 'icon-puzzle'
+      },
+      {
+        name: 'Forms',
+        url: '/base/forms',
+        icon: 'icon-puzzle'
+      },
+      {
+        name: 'Navbars',
+        url: '/base/navbars',
+        icon: 'icon-puzzle'
+
+      },
+      {
+        name: 'Pagination',
+        url: '/base/paginations',
+        icon: 'icon-puzzle'
+      },
+      {
+        name: 'Popovers',
+        url: '/base/popovers',
+        icon: 'icon-puzzle'
+      },
+      {
+        name: 'Progress',
+        url: '/base/progress',
+        icon: 'icon-puzzle'
+      },
+      {
+        name: 'Switches',
+        url: '/base/switches',
+        icon: 'icon-puzzle'
+      },
+      {
+        name: 'Tables',
+        url: '/base/tables',
+        icon: 'icon-puzzle'
+      },
+      {
+        name: 'Tabs',
+        url: '/base/tabs',
+        icon: 'icon-puzzle'
+      },
+      {
+        name: 'Tooltips',
+        url: '/base/tooltips',
+        icon: 'icon-puzzle'
+      }
+    ]
+  }
+];
